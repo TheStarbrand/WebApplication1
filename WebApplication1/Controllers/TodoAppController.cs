2aae94b98db1b7a5de85c9fc007036cd3dada4f3
Divine Atah <d_atah@u.pacific.edu>
2024-06-10 15:19:54 +0100

    Add project files.

---
+++ WebApplication1/Controllers/TodoAppController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.Extensions.Configuration;
+namespace WebApplication1.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TodoAppController : ControllerBase
+	{
+		private IConfiguration _configuration;
+		public TodoAppController(IConfiguration configuration)
+		{
+			_configuration = configuration;	
+		}
+
+		[HttpGet]
+		[Route("GetNotes")]
+		public JsonResult GetNotes()
+		{
+			string query = "select * from dbo.Notes";
+			DataTable table = new();
+			string sqlDataSource = _configuration.GetConnectionString("defaultConnection");
+			SqlDataReader myReader;
+
+			using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+			{
+				myCon.Open();
+				using (SqlCommand myCommand = new (query, myCon))
+				{
+					myReader = myCommand.ExecuteReader(); 
+					table.Load(myReader);
+					myReader.Close();
+					myCon.Close();
+				}
+			}
+
+			return new JsonResult(table);
+
+		}
+
+
+	}
+
+}
+
+
